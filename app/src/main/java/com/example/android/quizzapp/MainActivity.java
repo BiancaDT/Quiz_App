124d92e2ef2e1c5949feaf930b98ec62b2106483
BiancaDT <bianca.dianat@gmail.com>
2017-04-12 23:27:16 +0200

    Final commit :)

--- app/src/main/java/com/example/android/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizzapp/MainActivity.java
@@ -17,8 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.geo2);
     }
-
 
     // submitting answers button
     public void submitAnswers(View view) {
